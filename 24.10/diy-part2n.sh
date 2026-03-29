e9345500a2d4340365b98cec14894b9b774640b8
Jameslau-tech <Jameslau@126.com>
2026-03-29 10:36:17 +0800

    Update diy-part2n.sh

--- 24.10/diy-part2n.sh
+++ 24.10/diy-part2n.sh
@@ -105,7 +105,7 @@ if [ -f "$RUST_FILE" ]; then
 fi
 
 # 修改默认 IP (192.168.2.1)
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/JWRT/g' package/base-files/files/bin/config_generate
 
 echo "✅ SSH2 配置完成。"
