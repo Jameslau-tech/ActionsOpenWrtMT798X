4e2a6428c7e5fe2e71a36574d09226e0574f965b
Jameslau-tech <Jameslau@126.com>
2026-04-24 09:38:17 +0800

    Update diy-partX.sh

--- diy-partX.sh
+++ diy-partX.sh
@@ -5,6 +5,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+https://github.com/VIKINGYFY/CloseWRT-CI
 #https://github.com/bia-pain-bache/BPB-Worker-Panel
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
