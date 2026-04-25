7278a58b43c238ce450a31f7ffd3ec0dea0af5bc
Jameslau-tech <Jameslau@126.com>
2026-04-25 21:50:54 +0800

    Update diy-part1.sh

--- 25.12/diy-part1.sh
+++ 25.12/diy-part1.sh
@@ -26,7 +26,7 @@ rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 -b dev https://github.com/vernesong/OpenClash.git feeds/luci/applications/luci-app-openclash
 
 # Nikki / Momo
-git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git package/custom/nikki
+#git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git package/custom/nikki
 #git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo.git package/custom/momo
 
 # Daed
