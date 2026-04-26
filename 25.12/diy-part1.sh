7f666151b52c3a9a066704adfe88afaa342d6a40
Jameslau-tech <Jameslau@126.com>
2026-04-26 16:34:16 +0800

    Update diy-part1.sh

--- 25.12/diy-part1.sh
+++ 25.12/diy-part1.sh
@@ -41,7 +41,7 @@ git clone --depth=1 -b dev https://github.com/vernesong/OpenClash.git feeds/luci
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice.git package/custom/poweroffdevice
 #git clone --depth=1 https://github.com/isalikai/luci-app-owq-wol.git package/custom/owq-wol
 #git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/custom/lucky
-git clone --depth=1 https://github.com/sbwml/luci-app-openlist2.git package/custom/openlist2
+#git clone --depth=1 https://github.com/sbwml/luci-app-openlist2.git package/custom/openlist2
 
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog.git package/custom/watchdog
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan.git package/custom/taskplan
